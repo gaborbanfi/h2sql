1f4c39d7a298a9c7da54d8ec4bbcd34f9d808f83
GaborBanfi <banfi.gabor.90@gmail.com>
2018-09-17 20:10:06 +0200

    initial commit

---
+++ src/main/java/com/garage/sql/h2sql/H2SqlApplication.java
@@ -0,0 +1,12 @@
+package com.garage.sql.h2sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2SqlApplication.class, args);
+	}
+}
