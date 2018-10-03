6412742d577fcfbc85040aa7ab2b66754ddd03ea
GaborBanfi <banfi.gabor.90@gmail.com>
2018-10-03 18:32:41 +0200

    metrics+results

--- src/main/java/com/garage/sql/h2sql/controller/MeasureController.java
+++ src/main/java/com/garage/sql/h2sql/controller/MeasureController.java
@@ -2,6 +2,8 @@ package com.garage.sql.h2sql.controller;
 
 import com.garage.sql.h2sql.service.MeasureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +17,22 @@ public class MeasureController {
     }
 
     @GetMapping("/measure")
-    public long answerMe() {
-        return measureService.measureIndexedQuery();
+    public ResponseEntity answerMe() {
+        try {
+            measureService.measureIndexedQuery();
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping("/measure/slow")
-    public long answerMeSlowly() {
-        return measureService.measureNonIndexedQuery();
+    public ResponseEntity answerMeSlowly() {
+        try {
+            measureService.measureNonIndexedQuery();
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/garage/sql/h2sql/service/MeasureService.java
+++ src/main/java/com/garage/sql/h2sql/service/MeasureService.java
@@ -1,51 +1,26 @@
 package com.garage.sql.h2sql.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
 
 @Service
 public class MeasureService {
     private final QuestionService questionService;
+    private final Timer answerQuery;
 
     @Autowired
-    public MeasureService(QuestionService questionService) {
+    public MeasureService(QuestionService questionService, MeterRegistry meterRegistry) {
         this.questionService = questionService;
+        answerQuery = meterRegistry.timer("ANSWER_QUERY");
     }
 
-    public long measureNonIndexedQuery() {
-        return measure(false);
+    public void measureNonIndexedQuery() {
+        answerQuery.record(() -> questionService.getTheOnlyAnswer("2*2", false));
     }
 
-    public long measureIndexedQuery() {
-        return measure(true);
-    }
-
-    private long measure(boolean indexed) {
-        final String[] questions = { "why", "2*2", "the meaning of life" };
-        ExecutorService executorService = new ForkJoinPool();
-        List<Callable<Object>> callableList = Arrays.asList(
-                () -> questionService.getTheOnlyAnswer(questions[0], indexed),
-                () -> questionService.getTheOnlyAnswer(questions[1], indexed),
-                () -> questionService.getTheOnlyAnswer(questions[2], indexed)
-        );
-
-        long startTime = System.nanoTime();
-        long stopTime;
-        try {
-            for (int i = 0; i < 1000; i++) {
-                executorService.invokeAll(callableList);
-            }
-            stopTime = System.nanoTime();
-        } catch (InterruptedException e) {
-            return -1L;
-        }
-
-        return stopTime - startTime;
+    public void measureIndexedQuery() {
+        answerQuery.record(() -> questionService.getTheOnlyAnswer("2*2", true));
     }
 }
